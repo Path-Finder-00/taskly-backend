f66b54d5be1e36dd01d26d92d23124711aec5eb7
Lukasz <lukasz_golab_vib@wp.pl>
2024-05-07 00:29:36 +0200

    Small changes to comments endpoint

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,28 +4,30 @@ const { User, Employee, Project, Employee_Project, Ticket, Comment } = require('
 const { tokenExtractor } = require('../util/middleware')
 
 router.get('/:id', tokenExtractor, async (request, response) => {
-    // const user = await User.findOne({
-    //     where: {
-    //         id: request.decodedToken.id
-    //     },
-    //     include: [
-    //         {
-    //             model: Employee,
-    //             include: [
-    //                 Project
-    //             ]
-    //         }
-    //     ]
-    // })
-    // response.json(user.employee.projects)
+
     const ticket = await Ticket.findOne({
         where: {
             id: request.params.id
         },
         include: [
             {
-                model: Comment
+                model: Comment,
+                include: [
+                    {
+                        model: User,
+                        attributes: ['name', 'surname']
+                    }
+                ]
             }
+        ],
+        order: [
+            [
+                {
+                    model: Comment,
+                },
+                'createdAt',
+                'DESC'
+            ]
         ]
     })
     response.json(ticket.comments)
@@ -34,35 +36,26 @@ router.get('/:id', tokenExtractor, async (request, response) => {
 
 router.post('/:id', tokenExtractor, async (request, response) => {
 
-    // try {
-    //     const user = await User.findByPk(request.decodedToken.id)
-    //     const project = await Project.create({ name: request.body.name, description: request.body.description })
-    //     const project_manager = await Employee_Project.create({ since: new Date(), manager: true, employeeId: request.decodedToken.id, projectId: project.id })
-
-    //     request.body.employees.forEach(async employee => {
-    //         await Employee_Project.create({
-    //             since: new Date(),
-    //             manager: false,
-    //             employeeId: employee.id,
-    //             projectId: project.id,
-    //             roleId: employee.role_id
-    //         })
-    //     });
-
-    //     response.json(project);
-
-    // } catch (error) {
-    //     console.log(error)
-    //     return response.status(400).json({ error })
-    // }
     try {
         const comment = await Comment.create({
-            user_id: request.decodedToken.id,
-            ticket_id: request.params.id,
+            userId: request.decodedToken.id,
+            ticketId: request.params.id,
             comment: request.body.comment
         })
 
-        response.json(comment)
+        const newComment = await Comment.findOne({
+            where: {
+                id: comment.id
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['name', 'surname']
+                }
+            ]
+        })
+
+        response.json(newComment)
     } catch (error) {
         return response.status(400).json({ error })
     }
